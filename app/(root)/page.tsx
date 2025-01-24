64cbf64de2076359e2af113f02b4144c782e253f
subikstha <subsatch@gmail.com>
2025-01-24 08:16:48 +0545

    product list and product card component

--- app/(root)/page.tsx
+++ app/(root)/page.tsx
@@ -1,11 +1,20 @@
-// const delay = (ms: number) => new Promise((resolve) => setTimeout(resolve, ms));
-
 // export const metadata = {
 //   title: "Home",
+
+import ProductList from "@/components/shared/product/product-list";
+import sampleData from "@/db/sample-data";
+
 // };
-const HomePage = async () => {
-  // await delay(2000);
-  return <>Prostore</>;
+const HomePage = () => {
+  return (
+    <>
+      <ProductList
+        data={sampleData.products}
+        title="Newest Arrivals"
+        limit={4}
+      />
+    </>
+  );
 };
 
 export default HomePage;
--- components/shared/header/index.tsx
+++ components/shared/header/index.tsx
@@ -1,9 +1,7 @@
-import { Button } from "@/components/ui/button";
 import { APP_NAME } from "@/lib/constants";
-import { ShoppingCart, UserIcon } from "lucide-react";
 import Image from "next/image";
 import Link from "next/link";
-import ModeToggle from "./mode-toggler";
+import Menu from "./Menu";
 
 const Header = () => {
   return (
@@ -24,17 +22,7 @@ const Header = () => {
           </Link>
         </div>
         <div className="space-x-2">
-          <ModeToggle />
-          <Button asChild variant="ghost">
-            <Link href="/cart">
-              <ShoppingCart /> Cart
-            </Link>
-          </Button>
-          <Button asChild>
-            <Link href="/sign-in">
-              <UserIcon /> Sign In
-            </Link>
-          </Button>
+          <Menu />
         </div>
       </div>
     </header>

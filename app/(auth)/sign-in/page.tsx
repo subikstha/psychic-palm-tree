639c92c90a2979143d8ede0793c863dfeb35daa1
subikstha <subsatch@gmail.com>
2025-02-06 07:48:26 +0545

    sign in form

---
+++ app/(auth)/sign-in/page.tsx
@@ -0,0 +1,44 @@
+import {
+  Card,
+  CardContent,
+  CardDescription,
+  CardHeader,
+  CardTitle,
+} from "@/components/ui/card";
+import { APP_NAME } from "@/lib/constants";
+import { Metadata } from "next";
+import Image from "next/image";
+import Link from "next/link";
+import CredentialsSignInForm from "./credentials-signin-form";
+
+// This is going to change the title of the sign in page to Sign In | Prostore
+export const metadata: Metadata = {
+  title: "Sign In",
+};
+
+const SignInPage = () => {
+  return (
+    <div className="w-full max-w-md mx-auto">
+      <Card>
+        <CardHeader className="space-y-4">
+          <Link href="/" className="flex-center">
+            <Image
+              src="/images/logo.svg"
+              width={100}
+              height={100}
+              alt={`${APP_NAME} logo`}
+              priority={true}
+            />
+          </Link>
+          <CardTitle className="text-center">Sign In</CardTitle>
+          <CardDescription>Sign in to your account</CardDescription>
+        </CardHeader>
+        <CardContent className="space-y-4">
+          <CredentialsSignInForm />
+        </CardContent>
+      </Card>
+    </div>
+  );
+};
+
+export default SignInPage;
